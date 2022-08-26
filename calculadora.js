8a06c75a02fc2b8e4e3684212d379abe9b9871ca
Ximb <100860445+Ximbeh@users.noreply.github.com>
2022-08-26 18:46:02 -0300

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -3,8 +3,15 @@ function calcular(){
     var altura=(document.getElementById("q1").value)/100
     var peso=document.getElementById("q2").value
 
-
-    q3 = parseFloat(altura) / parseFloat(peso)*parseFloat(peso)
+    q3 = parseFloat(peso) / parseFloat(altura)**2
     document.getElementById('q3').value=q3
     console.log(q3)
 }
+
+function limpar(){
+    document.getElementById('q3').value=""
+    document.getElementById('q2').value=""
+    document.getElementById('q1').value=""
+}
+
+
